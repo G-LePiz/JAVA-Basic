437345a82b46a4a401ab2b9106c3f4929445b1f6
dawon <aodlstory321@gmail.com>
2025-01-08 21:16:04 +0900

    ê¸2주차 수정

--- src/week02/W02_Homework.java
+++ src/week02/W02_Homework.java
@@ -1,36 +1,67 @@
 package week02;
 
-import javax.swing.*;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class W02_Homework {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("저장할 형태를 입력하세요(Ex: List, Set, Map): ");
-        String str = sc.nextLine(); //저장할 형태
-        System.out.println("요리 제목을 입력하세요: ");
-        String cook = sc.nextLine(); //요리 제목
 
-        if(str.equals("List")){
-            ArrayList<String> cList = new ArrayList<>();//선언+생성
-            cList.add(cook);
+        System.out.println("저장할 자료형을 입력하세요(List/Set/Map): ");
+        String printlist = sc.nextLine(); // 저장할 자료형 입력받음
+        System.out.println(printlist); // 입력받은 자료형 출력
+        switch (printlist) {
 
-            ArrayList<String> strList = new ArrayList<>();//선언+생성
-            String book="";
-            while(!(book.equals("끝"))){
-                book = sc.nextLine();
-                strList.add(book);
-            }
-            System.out.println("[ "+str+" 로 저장된 "+cList.get(0)+" ]");
-            for(int i=0;i<strList.size()-1;i++){
-                if(!(strList.get(i)).equals("끝")){
-                    int box=i+1;
-                    System.out.println(box+". "+strList.get(i));
+            case "List" :
+                ArrayList<String> list = new ArrayList<>();
+                while(true){
+                    String text = sc.nextLine();
+                    if(text.equals("끝")) {
+                        break;
+                    }
+                    list.add(text);
                 }
-            }
+                System.out.println("[" + printlist + "으로 저장된 백종원 돼지고기 김치찌개 만들기");
+                for(int i=0; i< list.size(); i++) {
+                    int number = i+1;
+                    System.out.println(number + " : " + list.get(i) );
+                }
+                break;
+
+            case "Set":
+                Set<String> set = new HashSet<String>();
+                while (true) {
+                    String text = sc.nextLine();
+                    if(text.equals("끝")) {
+                        break;
+                    }
+                    set.add(text);
+                 }
+                System.out.println("[" + printlist + "으로 저장된 백종원 돼지고기 김치찌개 만들기");
+                Iterator<String> it = set.iterator();
+                Iterator iter = set.iterator();
+                for(int i=0; i< set.size(); i++) {
+                    int number = i+1;
+                    System.out.println(number + " : " + it.next());
+                }
+                break;
+
+            case "Map":
+                Map<Integer, String> map = new HashMap<>();
+                int startnumber = 1;
+                while (true) {
+                    String text = sc.nextLine();
+                    if(text.equals("끝")) {
+                        break;
+                    }
+                    map.put(startnumber ++, text);
+                }
+                System.out.println("[" + printlist + "으로 저장된 백종원 돼지고기 김치찌개 만들기");
+                for(int i=0; i< map.size(); i++) {
+                    int number = i+1;
+                    System.out.println(number + " : " + map.get(i+1));
+                }
+
+
         }
     }
 }
